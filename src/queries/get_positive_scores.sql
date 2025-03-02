a2d17e62810109dccbd1e86eebb1c8ac95d74530
Trung Dan Phan <trung-dan.phan@hec.edu>
2025-03-02 16:59:51 +0100

    :recycle: Refacto query positive scores

--- src/queries/get_positive_scores.sql
+++ src/queries/get_positive_scores.sql
@@ -1,11 +1,17 @@
 -- This query retrieves all users who rated mangas
 -- We remove scores of 0 which could indicate the manga hasn't been rated
 SELECT
-    uml.*,
+    uml.`Username`,
+    uml.`Manga Title`,
+    uml.`Genres`,
+    uml.`Format`,
+    uml.`Score`,
+    uml.`Progress`,
+    uml.`Status`,
     usr.`Score Format`
 FROM `manga-recommender-system.users_dataset.user_manga_list_raw` AS uml
 JOIN `manga-recommender-system.users_dataset.user_statistics_raw` AS usr
-ON uml.`User ID` = usr.`User ID`
+ON uml.`Username` = usr.`Username`
 WHERE `Score` > 0
 AND `Manga Title` is not null
 ;
