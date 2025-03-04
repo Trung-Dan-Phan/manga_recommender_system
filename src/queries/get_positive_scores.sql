9e5af3d82bcdda06ceea285cdea4423d617b5b91
Trung Dan Phan <trung-dan.phan@hec.edu>
2025-03-04 14:02:57 +0100

    :recycle: Refacto column name

--- src/queries/get_positive_scores.sql
+++ src/queries/get_positive_scores.sql
@@ -7,5 +7,5 @@ FROM `manga-recommender-system.users_dataset.user_manga_list_raw` AS uml
 JOIN `manga-recommender-system.users_dataset.user_statistics_raw` AS usr
 ON uml.`Username` = usr.`Username`
 WHERE `Score` > 0
-AND `Manga Title` is not null
+AND `Title Romaji` is not null
 ;
