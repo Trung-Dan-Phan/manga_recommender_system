49f67ea1102b20f5298e75cca5d8050d46d912f3
Trung Dan Phan <trung-dan.phan@hec.edu>
2025-03-12 16:07:01 +0100

    :rocket: Deploy workflows

--- src/training/workflows.py
+++ src/training/workflows.py
@@ -1,8 +1,8 @@
-import datetime
 import subprocess
 import time
 
 import prefect
+import psutil
 from loguru import logger
 from prefect import flow
 
@@ -15,8 +15,8 @@ from training.tasks import (
     train_baseline_models,
     train_knn_models,
     train_matrix_factorization_models,
+    write_data_task,
 )
-from utils.bigquery_utils import write_data_to_bigquery
 from utils.training_utils import get_best_model
 
 
@@ -26,20 +26,14 @@ def start_prefect_server():
     """
     try:
         # Check if the server is already running
-        result = subprocess.run(
-            ["pgrep", "-f", "prefect server start"], stdout=subprocess.PIPE
-        )
-        if result.stdout:
-            logger.info("Prefect server is already running.")
-            return
+        for process in psutil.process_iter(attrs=["pid", "name"]):
+            if "prefect" in process.info["name"]:
+                logger.info("Prefect server is already running.")
+                return
 
         # Start Prefect server
         logger.info("Starting Prefect server...")
-        subprocess.Popen(
-            ["prefect", "server", "start"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
+        subprocess.run("start cmd /k prefect server start", shell=True, check=True)
 
         # Wait a few seconds to ensure the server starts
         time.sleep(5)
@@ -50,7 +44,7 @@ def start_prefect_server():
 
 
 @flow(name="Fetching Manga Pipeline")
-def fetch_manga_data_pipeline():
+def fetch_manga_data_workflow():
     """
     Prefect flow that automates manga data fetching and storing.
     """
@@ -58,11 +52,9 @@ def fetch_manga_data_pipeline():
 
 
 @flow(name="Training Pipeline")
-def training_pipeline(query_path: str):
+def training_workflow(query_path: str):
     """
     Prefect Flow to load data, preprocess, train, and evaluate recommender models.
-
-    # TODO: Include preprocessing before training
 
     Args:
         query_path (str): Query path to fetch the dataset.
@@ -76,6 +68,10 @@ def training_pipeline(query_path: str):
     # Preprocess the data
     df = preprocessing_task(df)
 
+    # Select features for training
+
+    df = df[["username", "title_romaji", "normalized_score"]]
+
     # Train models in parallel
     matrix_future = train_matrix_factorization_models.submit(df)
     knn_future = train_knn_models.submit(df)
@@ -86,7 +82,7 @@ def training_pipeline(query_path: str):
     knn_future.result()
     baseline_future.result()
 
-    logger.success("Training Complete!")
+    logger.info("Training Complete!")
 
     logger.info("Check workflow by running `prefect server start`")
 
@@ -94,12 +90,12 @@ def training_pipeline(query_path: str):
 @flow(
     name="Recommendation Pipeline", retries=0
 )  # later: retries=1, retry_delay_seconds=5
-def recommendation_pipeline(username: str, top_n: int = 10):
+def recommendation_workflow(query_path: str, username: str, top_n: int = 10):
     """
     Prefect Flow to load data, train models, and make predictions.
 
     Args:
-        query_path (str): Query path to fetch the dataset.
+        query_path (str): Query path to fetch the processed dataset.
         username (str): Username for prediction.
         manga_title (str): Manga title for prediction.
     """
@@ -107,7 +103,7 @@ def recommendation_pipeline(username: str, top_n: int = 10):
     logger.info("Starting manga recommendation pipeline...")
 
     # Load the dataset
-    df = load_data_task(query_path)
+    df = load_data_task(query_path=query_path)
 
     try:
         # Get the best model
@@ -121,16 +117,26 @@ def recommendation_pipeline(username: str, top_n: int = 10):
         logger.info(f"Top {top_n} Recommendations for {username}:")
         logger.info(recommendations_df.head(top_n))
 
-        combined_df = load_data_task(query_path=f"{QUERY_PATH}/combine_users_manga.sql")
-
-        write_data_to_bigquery(
+        # Upload recommendations to BigQuery
+        write_data_task(
             dataset_id="recommendations_dataset",
             table_id="recommendations",
-            df=combined_df,
+            df=recommendations_df,
             mode="append",
         )
 
-        logger.success(f"Recommendations for {username} completed successfully")
+        # Combine recommendations with manga data
+        combined_df = load_data_task(query_path=f"{QUERY_PATH}/combine_users_manga.sql")
+
+        # Overwrite recommendations in BigQuery
+        write_data_task(
+            dataset_id="recommendations_dataset",
+            table_id="recommendations",
+            df=combined_df,
+            mode="overwrite",
+        )
+
+        logger.info(f"Recommendations for {username} completed successfully")
 
         logger.info("Check workflow by running `prefect server start`")
 
@@ -140,32 +146,22 @@ def recommendation_pipeline(username: str, top_n: int = 10):
 
 
 if __name__ == "__main__":
-    query_path = f"{QUERY_PATH}/simple_train_data.sql"
+    training_query_path = f"{QUERY_PATH}/get_positive_scores.sql"
+    recommendation_query_path = f"{QUERY_PATH}/simple_processed_users.sql"
     username = "Ruiisuuu"
     run_name = f"recommendations-for-{username}"
 
     start_prefect_server()
 
-    # Run the training pipeline
-    training_pipeline(query_path=query_path)
-
-    # Get recommendations for a specific user
-    recommendation_pipeline.with_options(name=run_name)(username=username)
-
-    # activate prefect using prefect server start in terminal.
-    # Then, open a new terminal and run workflow pipeline
-
-    # Run the Prefect flow periodically using Prefect’s scheduling system
-    from prefect.deployments import Deployment
-    from prefect.server.schemas.schedules import IntervalSchedule
-
-    # Schedule the flow to run daily
-    schedule = IntervalSchedule(interval=datetime.timedelta(hours=24))
-
-    deployment = Deployment.build_from_flow(
-        flow=fetch_manga_data_pipeline,
-        name="fetch_manga_data_pipeline",
-        schedule=schedule,
+    # Register flows for serving
+    training_workflow.serve(
+        name="train-model", parameters={"query_path": training_query_path}
     )
 
-    deployment.apply()
+    recommendation_workflow.serve(
+        name="generate-recommendations",
+        parameters={"query_path": recommendation_query_path, "username": username},
+    )
+
+    # Serve the workflow and schedule it to run every day at 6:00 AM
+    fetch_manga_data_workflow.serve(name="fetch-manga-data", cron="0 6 * * *")
