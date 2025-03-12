b6f9acb5d2bb814361c060ea7fae933e11a3b87f
Trung Dan Phan <trung-dan.phan@hec.edu>
2025-03-12 16:04:40 +0100

    :recycle: Refacto queries

--- src/queries/combine_users_manga.sql
+++ src/queries/combine_users_manga.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM `manga-recommender-system.recommendations_dataset.recommendations` AS recs
 JOIN `manga-recommender-system.manga_dataset.manga_collection_clean` AS manga
-ON recs.`title_romaji` = manga.`title_romaji`
+USING (`title_romaji`)
 ORDER BY `predicted_rating` DESC
