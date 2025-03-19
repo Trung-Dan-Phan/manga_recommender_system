064f1d46b9f7cca94fcb1e7ac7d045a41dc3ff72
Trung Dan Phan <trung-dan.phan@hec.edu>
2025-03-19 10:41:20 +0100

    :fire: Combine dataframes instead of using query

--- src/queries/combine_users_manga.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM `manga-recommender-system.recommendations_dataset.recommendations` AS recs
-JOIN `manga-recommender-system.manga_dataset.manga_collection_clean` AS manga
-USING (`title_romaji`)
-ORDER BY `predicted_rating` DESC
